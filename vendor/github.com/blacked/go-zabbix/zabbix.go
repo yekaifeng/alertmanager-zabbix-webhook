bd37cad38287a30bf11513d30b073b37d8018427
kennethye <kye@redhat.com>
2019-11-08 14:22:22 +0800

    fix nil pointer exception

--- vendor/github.com/blacked/go-zabbix/zabbix.go
+++ vendor/github.com/blacked/go-zabbix/zabbix.go
@@ -221,7 +221,7 @@ func (s *Sender) AlertSend(packet *AlertPacket) (res []byte, err error) {
 	// Get ip and port to zabbix host
 	iaddr, err := s.getTCPAddr()
 	if err != nil {
-		return nil, err
+		return
 	}
 
 	// New http client for Post
@@ -244,5 +244,5 @@ func (s *Sender) AlertSend(packet *AlertPacket) (res []byte, err error) {
 	}
 	fmt.Printf("response: %s:", string(content))
 
-	return content, err
+	return
 }
